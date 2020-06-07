1354ef7bb83006ed198203c318063c9fbdbedddf
Kirill <kirill.ksm22@gmail.com>
2020-06-07 22:34:50 +0500

    end work

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,4 @@
 let threed = document.querySelector('.modal-threed');
-let blur = document.querySelector('.blur-bg');
 
 let threedButton = document.querySelector('.three-d__btn');
 
@@ -8,7 +7,6 @@ let threedButtonClose = document.querySelector('.modal-threed-close');
 
 threedButton.onclick = function() {
   threed.classList.add('modal-threed--active');
-  blur.add('blur-bg--active');
 };
 
 threedButtonClose.onclick = function() {
